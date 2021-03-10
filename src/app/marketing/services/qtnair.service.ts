238547bb88f03bb85dd532da2220c7b47f7e4ab6
joy <joycok@gmail.com>
2021-03-10 12:09:36 +0530

    mrkt

--- src/app/marketing/services/qtnair.service.ts
+++ src/app/marketing/services/qtnair.service.ts
@@ -36,10 +36,39 @@ export class QtnAirService {
         private http2: HttpClient,
         private gs: GlobalService
     ) { }
+    public getSortCol(){
+        return this.record.sortcol;
+    }
+    public getSortOrder(){
+        return this.record.sortorder;
+    }
 
+    public getIcon(col : string){
+        if ( col == this.record.sortcol){
+          if ( this.record.sortorder )
+            return 'fa fa-arrow-down';
+          else 
+            return 'fa fa-arrow-up';
+        }
+        else 
+          return null;
+    }
+    
+    public  sort(col : string){
+        if ( col == this.record.sortcol){
+          this.record.sortorder = !this.record.sortorder;
+        }
+        else 
+        {
+          this.record.sortcol = col;
+          this.record.sortorder = true;
+        }
+    }
     public ClearInit()
     {
         this.record = <QtnmModel>{
+            sortcol : 'qtnm_no',
+            sortorder : true,
             errormessage: '',
             records: [],
             searchQuery: <SearchQuery>{ searchString: '', fromdate: this.gs.getPreviousDate(this.gs.SEARCH_DATE_DIFF), todate: this.gs.defaultValues.today, searchtype: 'Quote To / Quote #' },
@@ -61,6 +90,8 @@ export class QtnAirService {
         this.param_type = params.param_type;
 
         this.record = <QtnmModel>{
+            sortcol : 'qtnm_no',
+            sortorder : true,
             errormessage: '',
             records: [],
             searchQuery: <SearchQuery>{ searchString: '', fromdate: this.gs.getPreviousDate(this.gs.SEARCH_DATE_DIFF), todate: this.gs.defaultValues.today, searchtype: 'Quote To / Quote #' },
