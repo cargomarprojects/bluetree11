e8510a6e167de0109f06ca811e85f8a080481035
joy <joycok@gmail.com>
2021-03-12 15:29:01 +0530

    ft

--- src/app/usaccounts/fundtransfer/fundtransfer.component.ts
+++ src/app/usaccounts/fundtransfer/fundtransfer.component.ts
@@ -86,6 +86,41 @@ export class FundTransferComponent implements OnInit {
     this.gs.Naviagete('Silver.USAccounts.Trans/FundTransEditPage', JSON.stringify(parameter));
   }
 
+  getRouteDet(_type: string, _mode: string, _record: Tbl_Acc_Payment = null) {
+
+    if (_type == "L") {
+      if ((_mode == "ADD" && this.mainservice.canAdd) || (_mode == "EDIT" && this.mainservice.canEdit))
+        return "/Silver.USAccounts.Trans/FundTransEditPage";
+      else
+        return null;
+    } else if (_type == "P") {
+
+      if (_record == null) {
+        if (!this.mainservice.canAdd)
+          return null;
+        return {
+          appid: this.gs.appid,
+          menuid: this.mainservice.menuid,
+          pkid: '',
+          type: this.mainservice.param_type,
+          origin: 'accpayment-page',
+          mode: 'ADD'
+        };
+      }
+      if (!this.mainservice.canEdit)
+        return null;
+      return {
+        appid: this.gs.appid,
+        menuid: this.mainservice.menuid,
+        pkid: _record.pay_pkid,
+        type: '',
+        origin: 'acopen-page',
+        mode: 'EDIT'
+      };
+    } else
+      return null;
+  }
+
   Close() {    
     this.location.back();
   }
