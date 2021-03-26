24a1da18504026eda9506038fa74b06197230aff
Joy <joycok@gmail.com>
2021-03-26 15:00:23 +0530

    save

--- src/app/airexport/services/airexp-house.service.ts
+++ src/app/airexport/services/airexp-house.service.ts
@@ -32,7 +32,7 @@ export class AirExpHouseService {
 
 
     public initlialized: boolean;
-    private LSESSION = 0;
+    private appid = '';
     
     constructor(
         private http2: HttpClient,
@@ -79,9 +79,9 @@ export class AirExpHouseService {
         this.mdata$.next(this.record);
     }
     public init(params: any) {
-        if (this.LSESSION < this.gs.GSESSION)
+        if (this.appid != this.gs.appid)
         {
-            this.LSESSION = this.gs.GSESSION;
+            this.appid = this.gs.appid;
             this.initlialized = false;
         }
         if (this.initlialized)
--- src/app/airexport/services/searchpage.service.ts
+++ src/app/airexport/services/searchpage.service.ts
@@ -31,7 +31,7 @@ export class SearchPageService {
     public canDelete: boolean;
 
     public initlialized: boolean;
-    private LSESSION = 0;
+    private appid = '';
 
     constructor(
         private http2: HttpClient,
@@ -77,8 +77,8 @@ export class SearchPageService {
         this.mdata$.next(this.record);
     }
     public init(params: any) {
-        if (this.LSESSION < this.gs.GSESSION) {
-            this.LSESSION = this.gs.GSESSION;
+        if (this.appid != this.gs.appid) {
+            this.appid = this.gs.appid;
             this.initlialized = false;
         }
         if (this.initlialized)
