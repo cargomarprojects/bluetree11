48386a59a2c48e2c9791fe222fbf52249a5efb91
Joy <joycok@gmail.com>
2021-03-29 15:51:21 +0530

    save

--- src/app/core/services/interceptor.service.ts
+++ src/app/core/services/interceptor.service.ts
@@ -3,6 +3,8 @@ import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/c
 import { Observable } from 'rxjs';
 import { finalize } from "rxjs/operators";
 import { LoadingScreenService } from './loadingscreen.service';
+import { ActivationEnd, Router } from '@angular/router';
+
 
 
 @Injectable({
@@ -14,13 +16,32 @@ export class InterceptorService implements HttpInterceptor {
         '/to',
     ];
 
+    
+    sub: any;
+
     activeRequests: number = 0;
 
-    constructor(private loadingScreenService: LoadingScreenService) {
+    constructor(private loadingScreenService: LoadingScreenService,
+        router : Router
+    ) {
+
+        this.sub = router.events.subscribe(event => {
+            // An event triggered at the end of the activation part of the Resolve phase of routing.
+            if (event instanceof ActivationEnd ) {
+              // Cancel pending calls
+              this.activeRequests  =0;
+              this.loadingScreenService.stopLoading();
+            }
+        });
+    }
+
+    
+    ngOnDestroy() {
+        this.sub.unsusbscribe();
     }
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-
+            
         let displayLoadingScreen = true;
 
         for (const skippUrl of this.skippUrls) {
