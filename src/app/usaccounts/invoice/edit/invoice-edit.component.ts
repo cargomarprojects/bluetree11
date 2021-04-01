a7653516d3f66347b12b46b26ebca0a72ab62f62
joy <joycok@gmail.com>
2021-04-01 10:48:41 +0530

    inv

--- src/app/usaccounts/invoice/edit/invoice-edit.component.ts
+++ src/app/usaccounts/invoice/edit/invoice-edit.component.ts
@@ -732,20 +732,20 @@ export class InvoiceEditComponent implements OnInit {
     }
 
     //NEW
-    if (this.gs.isBlank(this.record.inv_arap)) {
-      this.errorMessage = "Invalid ARAP Type";
-      return false;
-    }
+    // if (this.gs.isBlank(this.record.inv_arap)) {
+    //   this.errorMessage = "Invalid ARAP Type";
+    //   return false;
+    // }
 
-    if (this.gs.isBlank(this.record.inv_type)) {
-      this.errorMessage = "Invalid Invoice Type";
-      return false;
-    }
+    // if (this.gs.isBlank(this.record.inv_type)) {
+    //   this.errorMessage = "Invalid Invoice Type";
+    //   return false;
+    // }
 
-    if (this.gs.isBlank(this.record.inv_mbl_id)) {
-      this.errorMessage = "Invalid Master ID";
-      return false;
-    }
+    // if (this.gs.isBlank(this.record.inv_mbl_id)) {
+    //   this.errorMessage = "Invalid Master ID";
+    //   return false;
+    // }
 
     let sErrMsg = "";
     let iCtr = 0;
@@ -790,9 +790,9 @@ export class InvoiceEditComponent implements OnInit {
         }
       }
 
-      if (this.gs.roundNumber2(Rec.invd_qty * Rec.invd_rate, 2) != this.gs.roundNumber2(Rec.invd_total, 2)) {
-        sErrMsg = "Row Amount Mismatch";
-      }
+      // if (this.gs.roundNumber2(Rec.invd_qty * Rec.invd_rate, 2) != this.gs.roundNumber2(Rec.invd_total, 2)) {
+      //   sErrMsg = "Row Amount Mismatch";
+      // }
 
     });
 
