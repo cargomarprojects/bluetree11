617b5bdfcf9e6bfafd82d9ae851428a322e86106
joy <joycok@gmail.com>
2021-04-16 13:51:49 +0530

    wt

--- src/app/other/wiretransfered/bank/wiretransfered-bank.component.ts
+++ src/app/other/wiretransfered/bank/wiretransfered-bank.component.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { GlobalService } from '../../../core/services/global.service';
 import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { SearchTable } from '../../../shared/models/searchtable';
-import { Tbl_Cargo_Wiretransferm,Tbl_Cargo_Wiretransferd } from '../../models/tbl_cargo_wiretransferm';
+import { Tbl_Cargo_Wiretransferm, Tbl_Cargo_Wiretransferd } from '../../models/tbl_cargo_wiretransferm';
 import { WireTransferedService } from '../../services/wiretransfered.service';
 
 @Component({
@@ -19,13 +19,13 @@ export class WireTransferedBankComponent implements OnInit {
     @Input() set mRecord(value: Tbl_Cargo_Wiretransferd) {
         this._mRecord = value;
     }
-     
+
     @Output() callbackevent = new EventEmitter<any>();
 
     modal: any;
     public records: Tbl_Cargo_Wiretransferd[] = [];
     public SelectedRecord: Tbl_Cargo_Wiretransferd = <Tbl_Cargo_Wiretransferd>{};
-     
+
 
     constructor(
         private modalconfig: NgbModalConfig,
@@ -52,7 +52,19 @@ export class WireTransferedBankComponent implements OnInit {
         SearchData.pkid = this._mRecord.cwd_beneficiary_id;
         this.mainservice.LoadGenList(SearchData).subscribe(response => {
             this.records = response.records;
-            this.modal = this.modalservice.open(bankmodal, { centered: true });
+            if (this.gs.isBlank(this.records))
+                alert("Bank Details Not Found");
+            else {
+                this.records.forEach(rec => {
+                        rec.pb_row_selected = false;
+                });
+                if (this.records.length == 1) {
+                    this._mRecord.cwd_beneficiary_bank_id = this.records[0].pb_pkid;
+                    this._mRecord.pb_bank_name = this.records[0].pb_bank_name;
+                    this._mRecord.pb_parent_id = this.records[0].pb_parent_id;
+                } else
+                    this.modal = this.modalservice.open(bankmodal, { centered: true });
+            }
 
         }, error => {
             alert(this.gs.getError(error));
@@ -61,18 +73,10 @@ export class WireTransferedBankComponent implements OnInit {
     }
 
     SelectRecord(_rec: Tbl_Cargo_Wiretransferd) {
-       
-        this.SelectedRecord = _rec;
-
-        // this.records.forEach(Rec => {
-        //     if (Rec.invd_payroll_date == _rec.invd_payroll_date) {
-        //         Rec.invd_payroll_yn = 'Y';
-        //         Rec.invd_payroll_b = true;
-        //     } else {
-        //         Rec.invd_payroll_yn = 'N';
-        //         Rec.invd_payroll_b = false;
-        //     }
-        // })
+        this._mRecord.cwd_beneficiary_bank_id = _rec.pb_pkid;
+        this._mRecord.pb_bank_name = _rec.pb_bank_name;
+        this._mRecord.pb_parent_id = _rec.pb_parent_id;
+        this.modal.close();
     }
 
     CloseModal(_type: string) {
