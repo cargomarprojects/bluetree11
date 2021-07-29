2a8ef8b5e206c44552a0fb59043947f9079fbaf6
joy <joycok@gmail.com>
2021-07-29 12:39:08 +0530

    edi2

--- src/app/importdata/linkpage/linkpage.component.ts
+++ src/app/importdata/linkpage/linkpage.component.ts
@@ -33,12 +33,13 @@ export class LinkPageComponent implements OnInit {
     }
 
     ngOnInit() {
+        this.gs.checkAppVersion();
         this.mainservice.init(this.route.snapshot.queryParams, this.mrecord);
         this.initPage();
     }
 
     initPage() {
-        this.gs.checkAppVersion();
+        
         this.records$ = this.mainservice.data$.pipe(map(res => res.records));
         this.searchQuery$ = this.mainservice.data$.pipe(map(res => res.searchQuery));
         this.pageQuery$ = this.mainservice.data$.pipe(map(res => res.pageQuery));
