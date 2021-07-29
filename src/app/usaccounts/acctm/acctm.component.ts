5c3b18f00550ec3b44e2210966cc627dbc3f45aa
Joy <joycok@gmail.com>
2021-07-29 11:28:43 +0530

    save

--- src/app/usaccounts/acctm/acctm.component.ts
+++ src/app/usaccounts/acctm/acctm.component.ts
@@ -34,12 +34,14 @@ export class AcctmComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+    this.gs.checkAppVersion();
     this.mainservice.init(this.route.snapshot.queryParams);
     this.initPage();
   }
 
   initPage() {
-    this.gs.checkAppVersion();
+    
+    
     this.records$ = this.mainservice.data$.pipe(map(res => res.records));
     this.searchQuery$ = this.mainservice.data$.pipe(map(res => res.searchQuery));
     this.pageQuery$ = this.mainservice.data$.pipe(map(res => res.pageQuery));
