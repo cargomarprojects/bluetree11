6bfab88b5e61032b2b2423fc2dc9b9fd756bab63
Joy <joycok@gmail.com>
2021-08-10 15:52:50 +0530

    save

--- src/app/core/login/login.component.ts
+++ src/app/core/login/login.component.ts
@@ -24,7 +24,7 @@ export class LoginComponent implements OnInit {
   errorMessage: string = '';
 
 
-  software_version_string = '1.116';
+  software_version_string = '1.117';
 
   username: string = '';
   password: string = '';
