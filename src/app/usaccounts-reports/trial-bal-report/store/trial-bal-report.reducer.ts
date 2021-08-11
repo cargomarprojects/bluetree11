105304c5b5c05687f3b593c362fd07d6f429cb5e
joy <joycok@gmail.com>
2021-08-11 11:33:12 +0530

    tb

--- src/app/usaccounts-reports/trial-bal-report/store/trial-bal-report.reducer.ts
+++ src/app/usaccounts-reports/trial-bal-report/store/trial-bal-report.reducer.ts
@@ -4,6 +4,7 @@ import { AppState as thisState } from '../../../reducer';
 
 import * as myActions from './trial-bal-report.actions';
 import { ReportState } from './trial-bal-report.models';
+import * as _ from 'lodash-es';
 
 export interface AppState extends thisState {
     'TrialBalReport': ReportState
@@ -27,6 +28,8 @@ export const initialState: ReportState = {
     filename2: '',
     filetype2: '',
     filedisplayname2: '',
+    sortcol: 'tb_acc_code',
+    sortorder: true,
     page_rows: 0,
     page_count: 0,
     page_current: 0,
@@ -42,6 +45,26 @@ export function TrialBalReportReducer(state: ReportState[] = [initialState], act
             return [...state.filter(rec => rec.urlid != action.payload.id), action.payload.changes];
         case myActions.ActionTypes.DELETE:
             return [...state.filter(rec => rec.urlid != action.payload.id)];
+        case myActions.ActionTypes.SORT_DATA: {
+
+            var st = Object.assign({}, state.find(rec => rec.urlid == action.payload.id));
+            if (st == null)
+                return [...state];
+
+            if (st.sortcol != action.payload.sortcol) {
+                st.sortcol = action.payload.sortcol;
+                st.sortorder = true;
+            }
+            else
+                st.sortorder = !st.sortorder;
+
+            if (st.sortorder)
+                st.records = _.orderBy(st.records, ['row_type', st.sortcol], ['asc', 'asc']);
+            else
+                st.records = _.orderBy(st.records, ['row_type', st.sortcol], ['asc', 'desc']);
+
+            return [...state.filter(rec => rec.urlid != action.payload.id), st];
+        }
         default:
             return state;
     }
