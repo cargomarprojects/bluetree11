7f86261f556d6e1b54c9b4d5059b3fdead4ff5dd
Joy <joycok@gmail.com>
2021-08-12 14:14:17 +0530

    savd

--- src/app/other/services/oth-general.service.ts
+++ src/app/other/services/oth-general.service.ts
@@ -126,6 +126,7 @@ export class OthGeneralService {
         this.record.errormessage = '';
         if (type == 'SEARCH') {
             this.record.searchQuery = _searchdata.searchQuery;
+            this.record.selectedId = '';            
         }
         if (type == 'PAGE') {
             this.record.pageQuery = _searchdata.pageQuery;
