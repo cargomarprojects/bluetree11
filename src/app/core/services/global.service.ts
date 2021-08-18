dc8f20212e7520dbb3dc4ed8d2750ec5880e506a
Joy <joycok@gmail.com>
2021-08-18 17:06:37 +0530

    save

--- src/app/core/services/global.service.ts
+++ src/app/core/services/global.service.ts
@@ -49,7 +49,7 @@ export class GlobalService {
 
   public appid = '';
 
-  public software_version_string = '1.122';
+  public software_version_string = '1.124';
   public baseUrl: string = "http://localhost:5000";
   //public baseUrl: string = "";
   // change this is false in production and update
