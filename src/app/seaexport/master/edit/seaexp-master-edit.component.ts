b0e3bd8c6c1cfd03f1b56d79173ca77c72efb328
joy <joycok@gmail.com>
2021-10-18 12:20:21 +0530

    algn

--- src/app/seaexport/master/edit/seaexp-master-edit.component.ts
+++ src/app/seaexport/master/edit/seaexp-master-edit.component.ts
@@ -18,6 +18,7 @@ import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { DateComponent } from '../../../shared/date/date.component';
 import { AutoComplete2Component } from '../../../shared/autocomplete2/autocomplete2.component';
 //EDIT-AJITH-06-09-2021
+//EDIT-AJITH-18-10-2021
 
 @Component({
   selector: 'app-seaexp-master-edit',
