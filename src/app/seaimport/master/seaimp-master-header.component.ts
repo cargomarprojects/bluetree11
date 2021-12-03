e0b221d8870340dc4f9ff093bccdd41a7f5bad71
joy <joycok@gmail.com>
2021-12-03 11:14:56 +0530

    si

--- src/app/seaimport/master/seaimp-master-header.component.ts
+++ src/app/seaimport/master/seaimp-master-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter,SimpleChange, ChangeDetectionStrategy,ViewChild } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, SimpleChange, ChangeDetectionStrategy, ViewChild } from '@angular/core';
 import { GlobalService } from '../../core/services/global.service';
 import { SearchQuery } from '../models/tbl_cargo_imp_masterm';
 //import { DateComponent } from '../../shared/date/date.component';
@@ -6,31 +6,35 @@ import { SearchQuery } from '../models/tbl_cargo_imp_masterm';
 @Component({
   selector: 'app-seaimp-master-header',
   templateUrl: './seaimp-master-header.component.html',
-  changeDetection : ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SeaImpMasterHeaderComponent implements OnInit {
   // Call By Value using Input Parameters
   //@ViewChild('_si_frm_date') si_frm_date_field: DateComponent;
-  
-  searchQuery : SearchQuery;
-  @Input() set _query( value : SearchQuery){
-    this.searchQuery  = Object.assign({}, value);
+
+  searchQuery: SearchQuery;
+  @Input() set _query(value: SearchQuery) {
+    this.searchQuery = Object.assign({}, value);
   }
-  
+
   @Output() searchEvents = new EventEmitter<any>();
 
   constructor(public gs: GlobalService
-    ) { }
+  ) { }
 
   ngOnInit() {
     //this.si_frm_date_field.Focus();
   }
 
-  ngOnChanges(changes:  SimpleChange ) {
+  ngOnChanges(changes: SimpleChange) {
+  }
+
+  onBlur(feild: string) {
+    this.searchQuery.searchString = this.searchQuery.searchString.toUpperCase();
   }
 
   List(outputformat: string) {
-    
+
     if (this.gs.isBlank(this.searchQuery.fromdate))
       this.searchQuery.fromdate = this.gs.year_start_date;
     if (this.gs.isBlank(this.searchQuery.todate))
