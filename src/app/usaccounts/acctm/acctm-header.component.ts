ac81f5511d705f361fe29542f277395b6a8a7ddf
joy <joycok@gmail.com>
2022-03-18 14:32:17 +0530

    actm

--- src/app/usaccounts/acctm/acctm-header.component.ts
+++ src/app/usaccounts/acctm/acctm-header.component.ts
@@ -29,4 +29,12 @@ export class AcctmHeaderComponent implements OnInit {
             this.searchQuery.searchString = '';
         this.searchEvents.emit({ outputformat: outputformat, searchQuery: this.searchQuery });
     }
+    onBlur(field: string) {
+        switch (field) {
+          case 'searchString': {
+            this.searchQuery.searchString = this.gs.trimAll(this.searchQuery.searchString.toUpperCase())
+            break;
+          }
+        }
+      }
 }
