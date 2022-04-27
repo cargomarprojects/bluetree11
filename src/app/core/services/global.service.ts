2eb7952fbb90fdf89bf4898439e002eb4668b947
Joy <joycok@gmail.com>
2022-04-27 16:04:14 +0530

    save

--- src/app/core/services/global.service.ts
+++ src/app/core/services/global.service.ts
@@ -54,7 +54,7 @@ export class GlobalService {
 
   public tab = ' 	';
 
-  public software_version_string = '1.162';
+  public software_version_string = '1.163';
 
   public baseUrl: string = "http://localhost:5000";
   //public baseUrl: string = "";
