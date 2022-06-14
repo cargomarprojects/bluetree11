5e25a34d8e1e0f7cdb6b1da73acbfb5b88871813
joy <joycok@gmail.com>
2022-06-14 15:37:17 +0530

    wh

---
+++ src/app/warehouse/outward/edit/wh-outward-det.component.ts
@@ -0,0 +1,121 @@
+import { Component, OnInit, Input, Output, EventEmitter, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { GlobalService } from '../../../core/services/global.service';
+import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Tbl_wh_inwardd } from '../../models/Tbl_wh_inwardm';
+import { WhOutwardService } from '../../services/whoutward.service';
+
+@Component({
+    selector: 'app-wh-outward-det',
+    templateUrl: './wh-outward-det.component.html'
+})
+export class WhOutwardDetComponent implements OnInit {
+
+    public errorMessage: string = '';
+    public tab: string = 'main';
+
+    private _mblid: string;
+    @Input() set mblid(value: string) {
+        this._mblid = value;
+    }
+
+    private _custid: string = '';
+    @Input() set custid(value: string) {
+        this._custid = value;
+    }
+
+    private _refno: string;
+    @Input() set refno(value: string) {
+        this._refno = value;
+    }
+
+    private _empname: string;
+    @Input() set empname(value: string) {
+        this._empname = value;
+    }
+    
+    private _recordexist: boolean;
+    @Input() set recordexist(value: boolean) {
+        this._recordexist = value;
+    }
+
+    @Output() callbackevent = new EventEmitter<any>();
+
+    modal: any;
+    public records: Tbl_wh_inwardd[] = [];
+    public SelectedRecord: Tbl_wh_inwardd = <Tbl_wh_inwardd>{};
+
+
+    constructor(
+        private modalconfig: NgbModalConfig,
+        private modalservice: NgbModal,
+        private http2: HttpClient,
+        private mainservice: WhOutwardService,
+        private gs: GlobalService) {
+        modalconfig.backdrop = 'static'; //true/false/static
+        modalconfig.keyboard = true; //true Closes the modal when escape key is pressed
+    }
+
+    ngOnInit() {
+        this.gs.checkAppVersion();
+    }
+
+    LoadList(payrollmodal: any = null) {
+
+        if (this.gs.isBlank(this._custid)) {
+            alert('Invalid Customer');
+            return;
+        }
+
+        if (this._recordexist)
+        {
+            alert('Cannot Copy, Record Exists');
+            return;
+        }
+
+        var SearchData = this.gs.UserInfo;
+        SearchData.DCODE = this.gs.PAYROLL_INVOICE_CODE;
+        SearchData.ACODE = this.gs.PAYROLL_ACC_CODE;
+        SearchData.MBL_ID = this._mblid;
+        SearchData.CUST_ID = this._custid;
+        this.mainservice.GetProductdetails(SearchData).subscribe(response => {
+            this.records = response.list;
+            // this.records.forEach(rec => {
+            //     if (rec.invd_payroll_yn == 'Y')
+            //         rec.invd_payroll_b = true;
+            //     else
+            //         rec.invd_payroll_b = false;
+            // });
+            this.modal = this.modalservice.open(payrollmodal, { centered: true });
+
+        }, error => {
+            alert(this.gs.getError(error));
+        });
+
+    }
+
+    SelectRecord(_rec: Tbl_wh_inwardd) {
+        // _rec.invd_payroll_yn = 'Y';
+        // this.SelectedRecord = _rec;
+
+        // this.records.forEach(Rec => {
+        //     if (Rec.invd_payroll_date == _rec.invd_payroll_date) {
+        //         Rec.invd_payroll_yn = 'Y';
+        //         Rec.invd_payroll_b = true;
+        //     } else {
+        //         Rec.invd_payroll_yn = 'N';
+        //         Rec.invd_payroll_b = false;
+        //     }
+        // })
+    }
+
+    CloseModal(_type: string) {
+
+        if (this.callbackevent)
+            this.callbackevent.emit({ action: _type, rec: this.SelectedRecord });
+
+        this.modal.close();
+    }
+
+
+}
--- src/app/warehouse/services/whoutward.service.ts
+++ src/app/warehouse/services/whoutward.service.ts
@@ -230,6 +230,10 @@ export class WhOutwardService {
         return this.http2.post<any>(this.gs.baseUrl + '/api/Warehouse/Outward/Save', SearchData, this.gs.headerparam2('authorized'));
     }
      
+    GetProductdetails(SearchData: any) {
+        return this.http2.post<any>(this.gs.baseUrl + '/api/Warehouse/Outward/GetProductdetails', SearchData, this.gs.headerparam2('authorized'));
+    }
+
     DeleteRecord(SearchData: any) {
         return this.http2.post<any>(this.gs.baseUrl + '/api/Warehouse/Outward/DeleteRecord', SearchData, this.gs.headerparam2('authorized'));
     }
