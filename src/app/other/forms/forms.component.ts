8fdc5ee68044ad4a31ad3d255fb0b51a94b251fb
joy <joycok@gmail.com>
2023-06-05 15:17:07 +0530

    frms

--- src/app/other/forms/forms.component.ts
+++ src/app/other/forms/forms.component.ts
@@ -3,7 +3,7 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-
+import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { GlobalService } from '../../core/services/global.service';
 import { SearchQuery, Tbl_cargo_genfilesModel, Tbl_cargo_genfiles } from '../models/Tbl_cargo_genfiles';
 import { PageQuery } from '../../shared/models/pageQuery';
@@ -18,18 +18,24 @@ export class FormsComponent implements OnInit {
   /*
    Joy
  */
-
+  attach_parentid: string = "";
+  modal: any;
   errorMessage$: Observable<string>;
   records$: Observable<Tbl_cargo_genfiles[]>;
   pageQuery$: Observable<PageQuery>;
   searchQuery$: Observable<SearchQuery>;
 
   constructor(
+    private modalconfig: NgbModalConfig,
+    private modalservice: NgbModal,
     private route: ActivatedRoute,
     private location: Location,
     public gs: GlobalService,
     public mainservice: FormsService
-  ) { }
+  ) {
+    modalconfig.backdrop = 'static'; //true/false/static
+    modalconfig.keyboard = true; //true Closes the modal when escape key is pressed
+  }
 
   ngOnInit() {
     this.gs.checkAppVersion();
@@ -38,7 +44,7 @@ export class FormsComponent implements OnInit {
   }
 
   initPage() {
-    
+
     this.records$ = this.mainservice.data$.pipe(map(res => res.records));
     this.searchQuery$ = this.mainservice.data$.pipe(map(res => res.searchQuery));
     this.pageQuery$ = this.mainservice.data$.pipe(map(res => res.pageQuery));
@@ -61,7 +67,7 @@ export class FormsComponent implements OnInit {
     }
 
     let parameter = {
-      appid:this.gs.appid,
+      appid: this.gs.appid,
       menuid: this.mainservice.menuid,
       pkid: '',
       type: this.mainservice.param_type,
@@ -79,7 +85,7 @@ export class FormsComponent implements OnInit {
     }
 
     let parameter = {
-      appid:this.gs.appid,
+      appid: this.gs.appid,
       menuid: this.mainservice.menuid,
       pkid: _record.gf_pkid,
       type: '',
@@ -128,17 +134,29 @@ export class FormsComponent implements OnInit {
     this.location.back();
   }
 
-  ShowFile(_rec: Tbl_cargo_genfiles) {
-
-    let filename: string = "";
-    let filedisplayname: string = "";
-    filename = this.gs.FS_APP_FOLDER + _rec.gf_file_path + _rec.gf_file_id;
-    filedisplayname = _rec.gf_file_name;
-    if (_rec.gf_file_path != "")
-      this.Downloadfile(filename, "", filedisplayname);
+  ShowFile(_rec: Tbl_cargo_genfiles, attachmodal: any = null) {
+    if (_rec.gf_category == 'PRE-BOOKING-DOC') {
+      this.attach_parentid = _rec.gf_pkid;
+      this.modal = this.modalservice.open(attachmodal, { centered: true });
+    } else {
+      let filename: string = "";
+      let filedisplayname: string = "";
+      filename = this.gs.FS_APP_FOLDER + _rec.gf_file_path + _rec.gf_file_id;
+      filedisplayname = _rec.gf_file_name;
+      if (_rec.gf_file_path != "")
+        this.Downloadfile(filename, "", filedisplayname);
+    }
   }
 
   Downloadfile(filename: string, filetype: string, filedisplayname: string) {
     this.gs.DownloadFile(this.gs.FS_APP_FOLDER, filename, filetype, filedisplayname);
   }
+  CloseModal() {
+    this.modal.close();
+   
+  }
+  callbackevent(event: any) {
+
+  }
+
 }
