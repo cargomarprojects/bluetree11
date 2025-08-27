8955519623008a24d4c1e57ae48a34bc6bddb715
Ajith <ajithkvs@gmail.com>
2025-08-27 11:55:19 +0530

    aeagntcd

--- src/app/airexport/master/edit/airexp-master-edit.component.ts
+++ src/app/airexport/master/edit/airexp-master-edit.component.ts
@@ -452,6 +452,7 @@ export class AirExpMasterEditComponent implements OnInit {
 
     if (_Record.controlname == "AGENT") {
       this.record.mbl_agent_id = _Record.id;
+      this.record.mbl_agent_code = _Record.code;
       this.record.mbl_agent_name = _Record.name;
       if (!this.gs.isBlank(this.mbl_frt_status_field))
         this.mbl_frt_status_field.nativeElement.focus();
