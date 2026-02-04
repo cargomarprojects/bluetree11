48b29a24d8211cf6fc3a025f0e25160e9f0d0d99
Joy <joy@cargomar.in>
2026-02-04 14:05:13 +0530

    save

--- src/app/core/services/global.service.ts
+++ src/app/core/services/global.service.ts
@@ -55,7 +55,7 @@ export class GlobalService {
 
   public tab = ' 	';
 
-  public software_version_string = '1.276';
+  public software_version_string = '1.277';
 
   public baseUrl: string = "http://localhost:5000";
   //public baseUrl: string = "";
