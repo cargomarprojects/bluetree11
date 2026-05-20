e952d5f617dcc25f607d5f7ffc2c988b6448138f
Ajith <ajithkvs@gmail.com>
2026-05-20 15:58:12 +0530

    rpt

--- src/app/shared/report2/report2.component.ts
+++ src/app/shared/report2/report2.component.ts
@@ -146,23 +146,7 @@ export class Report2Component implements OnInit {
   }
 
   report(action: string, emailmodal: any = null) {
-
-    // if (action == "email") {
-    //   this.Mail_Pkid = this.gs.getGuid();
-    //   this.AttachList = new Array<any>();
-    //   this.AttachList.push({ filename: this._filename, filetype: this._filetype, filedisplayname: this._filedisplayname });
-    //   this.modal = this.modalservice.open(emailmodal, { centered: true });
-    // }
-    // else if (action == "excel") {
-    //   if (this._filedisplayname2 == null || this._filedisplayname2 == undefined || this._filedisplayname2 == "")
-    //     return;
-    //   this.gs.DownloadFile(this.gs.GLOBAL_REPORT_FOLDER, this._filename2, this._filetype2, this._filedisplayname2);
-    // }
-    // else if (action == "download") {
-    //   if (this._filedisplayname == null || this._filedisplayname == undefined || this._filedisplayname == "")
-    //     return;
-    //   this.gs.DownloadFile(this.gs.GLOBAL_REPORT_FOLDER, this._filename, this._filetype, this._filedisplayname);
-    // }
+   
   }
 
   mailcallbackevent(event: any) {
